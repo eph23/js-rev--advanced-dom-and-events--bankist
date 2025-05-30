3f92ae83130b65326406f6cca53ec354de4e02c1
ephraim <ephraims.818@gmail.com>
2025-05-30 13:45:11 +0600

    JS-REV--ADVANCED-DOM-AND-EVENTS--PROJECT#03-BANKIST - 05. Manu Fade Animation

---
+++ 05. Menu Fade Animation/script.js
@@ -0,0 +1,103 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const openModal = function (event) {
+  event.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+btnScrollTo.addEventListener('click', function (event) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document
+  .querySelector('.nav__links')
+  .addEventListener('click', function (event) {
+    if (event.target.classList.contains('nav__link')) {
+      event.preventDefault();
+      const id = event.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+
+tabsContainer.addEventListener('click', function (event) {
+  const clicked = event.target.closest('.operations__tab');
+
+  if (!clicked) {
+    return;
+  }
+
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+const handleHover = function (event, opacity) {
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    //   siblings.forEach(element => {
+    //     if (element !== link) {
+    //       element.style.opacity = opacity;
+    //     }
+    //   });
+    //   logo.style.opacity = opacity;
+    // }
+    siblings.forEach(element => {
+      if (element !== link) {
+        element.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// nav.addEventListener('mouseover', function (event) {
+//   handleHover(event, 0.5);
+// });
+// nav.addEventListener('mouseout', function (event) {
+//   handleHover(event, 1);
+// });
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
